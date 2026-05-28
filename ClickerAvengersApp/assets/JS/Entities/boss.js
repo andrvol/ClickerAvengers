58342554818ac3ece77e7358102d4d48d6de09c7
andrvol <v0l0b.and0716@gmail.com>
2026-05-28 22:54:49 +0300

    Провел незначительные улучшения. Надо доработать главное меню

--- ClickerAvengersApp/assets/JS/Entities/boss.js
+++ ClickerAvengersApp/assets/JS/Entities/boss.js
@@ -1,6 +1,6 @@
 import { Monster } from "./monster.js";
 
-class Boss extends Monster {
+export class Boss extends Monster {
     constructor(passiveImg, onHitImg, deathImg, deathSound, hp, secondsToKill) {
         super(passiveImg, onHitImg, deathImg, deathSound, hp);
 
