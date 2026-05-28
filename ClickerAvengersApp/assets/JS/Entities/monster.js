00c29a5d9cb56dfbd81b31ca063bb6277d2ee555
andrvol <v0l0b.and0716@gmail.com>
2026-05-28 22:18:45 +0300

    Добавил класс монстр

---
+++ ClickerAvengersApp/assets/JS/Entities/monster.js
@@ -0,0 +1,10 @@
+class Monster {
+    constructor(passiveImg, onHitImg, deathImg, deathSound, hp) {
+        this.passiveImg = passiveImg;
+        this.onHitImg = onHitImg;
+        this.deathImg = deathImg;
+        this.deathSound = deathSound;
+
+        this.hp = hp; // string
+    }
+}
