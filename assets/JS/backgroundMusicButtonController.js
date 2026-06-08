6dc161a915393ca6d753bc10e272060ed11a825f
andrvol <v0l0b.and0716@gmail.com>
2026-06-08 18:40:17 +0300

    Добавил 2ой трек для фона + плавный переход между ними(доп ТЗ от препода)

--- assets/JS/backgroundMusicButtonController.js
+++ assets/JS/backgroundMusicButtonController.js
@@ -1,9 +1,13 @@
-const music = new Audio('./assets/audio/background-music.mp3');
+let currenTrack = 1;
 
-music.loop = true;
-music.volume = Number(localStorage.getItem('musicVolume')) || 0.2;
+let music = new Audio();
+music.loop = false;
+
+const baseVolume = Number(localStorage.getItem('musicVolume')) || 0.2;
 music.muted = localStorage.getItem('musicMuted') === 'true';
 
+music.addEventListener('ended', playNextTrack);
+music.addEventListener('timeupdate', handleFadeOut);
 
 export function updateBackgroundMusicBtn() {
     const musicBtn = document.getElementById('musicBtn');
@@ -20,7 +24,24 @@ export function updateBackgroundMusicBtn() {
 }
 
 function startMusic() {
-    music.play();
+    playCurrentTrack();
+}
+
+function playCurrentTrack() {
+    music.src = `./assets/audio/background-music${currenTrack}.mp3`;
+    music.load();
+
+    music.volume = 0;
+    delete music.dataset.fadingOut;
+
+    music.play().then(() => {
+        fadeIn();
+    });
+}
+
+function playNextTrack() {
+    currenTrack = currenTrack === 1 ? 2 : 1;
+    playCurrentTrack();
 }
 
 function toggleMusic(btn) {
@@ -35,4 +56,53 @@ function updateIcon(btn) {
     btn.src = music.muted
         ? './assets/images/icons-elements/sound-off.webp'
         : './assets/images/icons-elements/sound-on.webp';
+}
+
+function fadeIn() {
+    const duration = 10000;
+    const stepTime = 200;
+    const steps = duration / stepTime;
+
+    let step = 0;
+
+    const interval = setInterval(() => {
+        step++;
+
+        const progress = step / steps;
+        music.volume = baseVolume * progress;
+
+        if (step >= steps) {
+            music.volume = baseVolume;
+            clearInterval(interval);
+        }
+    }, stepTime);
+}
+
+function handleFadeOut() {
+    if (!music.duration) return;
+
+    const timeLeft = music.duration - music.currentTime;
+
+    if (timeLeft <= 10 && !music.dataset.fadingOut) {
+        music.dataset.fadingOut = "true";
+
+        const duration = 10000;
+        const stepTime = 200;
+        const steps = duration / stepTime;
+
+        let step = 0;
+        let startVolume = music.volume;
+
+        const interval = setInterval(() => {
+            step++;
+
+            const progress = step / steps;
+            music.volume = startVolume * (1 - progress);
+
+            if (step >= steps) {
+                music.volume = 0;
+                clearInterval(interval);
+            }
+        }, stepTime);
+    }
 }
